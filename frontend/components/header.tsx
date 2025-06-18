a65f56075232b9eda65b404f5ee3627fec840078
bg489 <plopro09@gmail.com>
2025-06-18 07:49:44 +0700

    up

--- frontend/components/header.tsx
+++ frontend/components/header.tsx
@@ -49,7 +49,7 @@ export function Header() {
               <Heart className="w-6 h-6 text-white" />
             </div>
             <div>
-              <h1 className="text-xl font-bold text-gray-900">BloodConnect</h1>
+              <h1 className="text-xl font-bold text-gray-900">ScαrletBlood    </h1>
               <p className="text-sm text-gray-600">Trung tâm Hiến máu Nhân đạo</p>
             </div>
           </Link>
