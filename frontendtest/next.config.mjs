a33a78fdc4468f3acb3b1fe4e1a4d143bb79674b
BaoCute <giabao123963@gmail.com>
2025-06-19 14:58:15 +0700

    test render

--- frontendtest/next.config.mjs
+++ frontendtest/next.config.mjs
@@ -10,6 +10,10 @@ const nextConfig = {
     unoptimized: true,
   },
   output: "export",
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
 }
 
 export default nextConfig
