92d2e69ae94cc04f7559a7e9d77627d46f36f0ef
BaoCute <giabao123963@gmail.com>
2025-06-19 15:15:02 +0700

    test render3

--- frontendtest/next.config.mjs
+++ frontendtest/next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const nextConfig = {
   eslint: {
